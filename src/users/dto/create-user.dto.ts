bce23ce7c579be54c85a9765d58fab0a5cc4e194
Alan Kydd <kydd.alan.g@gmail.com>
2026-05-25 18:02:20 -0600

    Add payload validation for CreateUserDto
    Need to format the error payload.

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,9 +1,21 @@
-export class CreateUserDto {
-	user: CreateUserDtoInner
-}
+import { IsDefined, IsEmail, IsNotEmpty, IsString, IsStrongPassword, ValidateNested } from "class-validator"
+import { Type } from "class-transformer"
 
 class CreateUserDtoInner {
+	@IsString()
+	@IsNotEmpty()
 	readonly username: string
+
+	@IsEmail()
 	readonly email: string
+
+	@IsStrongPassword()
 	readonly password: string
+}
+
+export class CreateUserDto {
+	@IsDefined()
+	@ValidateNested()
+	@Type(() => CreateUserDtoInner)
+	user: CreateUserDtoInner
 }
